c23251a6de18f25477c3a08c6dc7d7adc20fabf8
antoine-de <antoine.desbordes@gmail.com>
2020-06-30 15:59:42 +0200

    add a 10m timeout on jobs

--- gtfs_converter/api.py
+++ gtfs_converter/api.py
@@ -37,6 +37,7 @@ def _convert(conversion_type):
                 "task_date": datetime.datetime.today(),
                 "conversion_type": conversion_type,
             },
+            job_timeout="10m"
         )
         logging.info(
             f"Enquing {url} for datagouv_id {datagouv_id}, for {conversion_type} conversion(s)"
